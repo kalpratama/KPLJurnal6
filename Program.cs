7d1b56527a4ae1debb7bf10c0d649274bf682df4
kalpratama <kalpratama@gmail.com>
2024-03-25 06:44:21 +0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul6_1302220096
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
